981f44ab17065a84d6bcbe435d84173f2750741c
albertodelviejo <albertodelviejosanchez@gmail.com>
2020-08-01 18:25:00 +0200

    Creada MainActivity

---
+++ app/src/main/java/com/example/platziconf/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.platziconf
+
+import androidx.appcompat.app.AppCompatActivity;
+
+class MainActivity : AppCompatActivity() {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_mail)
+}
